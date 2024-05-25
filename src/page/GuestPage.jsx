a965a8bcbe8045eff228dbb395cb0bbafcdf63f9
chheng <chheng@gmail.com>
2024-05-25 21:55:28 +0700

    changed somthing

--- src/page/GuestPage.jsx
+++ src/page/GuestPage.jsx
@@ -10,7 +10,7 @@ const botPhoto = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
       <div className=' grid grid-cols-7 gap-6'>
       {
         botPhoto.map((b)=>(
-          <img src={`./src/assets/images/botRin${b}.jpg`} alt="" className=' blur-sm hover:blur-none object-contain w-[10rem] h-[15rem]' />
+          <img src={`./images/botRin${b}.jpg`} alt="" className=' blur-sm hover:blur-none object-contain w-[10rem] h-[15rem]' />
          ))
       }
       </div>
