9602dde70b67f11c657784f3cb7f7108cec7fdd6
chheng <chheng@gmail.com>
2024-07-11 18:45:05 +0700

    fix bug

--- src/components/ConnectToEMQX.jsx
+++ src/components/ConnectToEMQX.jsx
@@ -213,7 +213,7 @@ const ConnectToEMQX = ({ connect, disconnect, connectBtn }) => {
             {connectBtn}
           </button>
           <button
-            className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 rounded-lg font-semibold  sm:text-[0.9rem] sm:w-[45%] text-[1.1rem] w-[40%] px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+            className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 rounded-lg font-semibold  sm:text-[0.9rem] sm:w-[48%] text-[1.1rem] w-[40%] px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
             onClick={handleDisconnect}
           >
             Disconnect
