6717c055cee44ad1b21e7f3e28e61d292ac45dfd
chheng <chheng@gmail.com>
2024-07-12 01:21:16 +0700

    first commit

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,1 +1,2 @@
 export const base_url = "https://temperature-humidity-expressjs-api.onrender.com/data";
+export const base_url_from_ow = "https://temperature-store-data-from-openweather.onrender.com/data";
